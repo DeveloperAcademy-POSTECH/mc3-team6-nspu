7d40d48ab154ffd69e55c5ca37556829a10b330a
psangwon62 <psangwon62@gmail.com>
2023-07-12 12:29:41 +0900

    [CHORE]: 각 폴더별 더미파일 생성
    - Models/UserModel.swift 생성
    - UI/Components/HeaderView.swift 생성
    - Utilities/Extension+Color.swift 생성

---
+++ OrrNyan/OrrNyan/Models/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  OrrNyan
+//
+//  Created by 박상원 on 2023/07/12.
+//
+
+import Foundation
---
+++ OrrNyan/OrrNyan/Utilities/Extension+Color.swift
@@ -0,0 +1,8 @@
+//
+//  Extension+Color.swift
+//  OrrNyan
+//
+//  Created by 박상원 on 2023/07/12.
+//
+
+import Foundation
