9befc6a3f319c551ec918be83e0e888d36aaa444
psangwon62 <psangwon62@gmail.com>
2023-07-12 12:20:19 +0900

    [CHORE]: 기본 폴더 구조 생성
    - Model, UI, Resource, Utilites 폴더 생성
    - UI/Screen/Main에 MainView 저장

---
+++ OrrNyan/OrrNyan/UI/Screen/Main/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  OrrNyan
+//
+//  Created by 박상원 on 2023/07/12.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
