4b15adb2180e09744a01fda7bef79e9edaaa76d0
psangwon62 <psangwon62@gmail.com>
2023-07-18 10:46:11 +0900

    feat: Dummy HeaderView 생성
    - 뷰 위치 잡는 용 하드코딩된 HeaderView 생성

---
+++ OrrNyan/OrrNyan/UI/Screen/StageView.swift
@@ -0,0 +1,22 @@
+//
+//  StageView.swift
+//  OrrNyan
+//
+//  Created by 박상원 on 2023/07/14.
+//
+
+import SwiftUI
+
+struct StageView: View {
+    var body: some View {
+        VStack {
+            HeaderView()
+        }
+    }
+}
+
+struct StageView_Previews: PreviewProvider {
+    static var previews: some View {
+        StageView()
+    }
+}
