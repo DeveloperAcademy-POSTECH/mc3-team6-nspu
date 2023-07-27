54302d748f421119b3ee6d027d6104495295104d
zyeongPark <piiy10141997@gmail.com>
2023-07-27 14:34:09 +0900

    [Refactor] mainView에서 Element들이 사방에서 들어오도록 코드 수정했습니다

--- OrrNyan/OrrNyan/UI/Screen/Main/MainParallaxView.swift
+++ OrrNyan/OrrNyan/UI/Screen/Main/MainParallaxView.swift
@@ -9,31 +9,48 @@ import SwiftUI
 
 struct MainParallaxView: View {
     @EnvironmentObject var stageViewModel: StageViewModel
-    var arr = ["1", "2", "3"]
+//    var arr = ["1", "2", "3"]
     var nameSpace: Namespace.ID
     var body: some View {
         ZStack{
-            Image("StageBg01").ignoresSafeArea()
-            ForEach(arr, id: \.self){ item in
-                Image(item)
-            }
-            FrameInAnimation()
-            Image("StageEm0\(stageViewModel.selectedIndex + 1)_01")
-//                Image("StageEm0\(stageViewModel.selectedIndex + 1)_02")
-//                Image("StageEm0\(stageViewModel.selectedIndex + 1)_03")
+            //            ForEach(arr, id: \.self){ item in
+            //                Image(item)
+            //            }
+            BackOpacityAnimation()
+            FrameSideInAnimation()
+            FrameUpAnimation()
             Image("StageEm0\(stageViewModel.selectedIndex + 1)_04")
+            //랜드마크 이미지
             Image("StageSt0\(stageViewModel.selectedIndex + 1)")
                 .resizable()
                 .matchedGeometryEffect(id: "StageStImage0\(stageViewModel.selectedIndex + 1)", in: nameSpace)
                 .aspectRatio(contentMode: .fit)
-                .frame(width: UIScreen.main.bounds.width*0.75)
+                .padding(.top,20)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height*0.8)
                 .shadow(radius: 5)
+                .border(.red)
 
         }
     }
 }
 
-struct FrameInAnimation: View {
+struct FrameUpAnimation: View {
+    @EnvironmentObject var stageViewModel: StageViewModel
+    @State private var bottomImageOffset: CGFloat = UIScreen.main.bounds.height
+
+    var body: some View {
+        Image("StageEm0\(stageViewModel.selectedIndex + 1 )_01") // 새로운 이미지 파일의 이름으로 변경하세요
+            .scaledToFit()
+            .offset( y: bottomImageOffset)
+            .onAppear {
+                withAnimation(Animation.easeInOut(duration: 1.5)) {
+                    bottomImageOffset = 0
+                }
+            }
+    }
+}
+
+struct FrameSideInAnimation: View {
     @EnvironmentObject var stageViewModel: StageViewModel
     //맨 처음에 화면 양 옆에 숨어있도록 뷰 위치 설정
     @State private var leftImageOffset: CGFloat = -300
@@ -42,24 +59,33 @@ struct FrameInAnimation: View {
     var body: some View {
         ZStack {
             //왼쪽에서 들어오는 이미지
-            Image("StageEm01_02")
+            Image("StageEm0\(stageViewModel.selectedIndex + 1)_02")
                 .resizable()
                 .scaledToFill()
                 .frame(width: (UIScreen.main.bounds.width)*1.0, height: (UIScreen.main.bounds.height) * 1.0)
                 .offset(x: leftImageOffset, y: 0 )
-                .animation(.easeInOut(duration: 1.5))
             //오른쪽에서 들어오는 이미지
-            Image("StageEm01_03")
+            Image("StageEm0\(stageViewModel.selectedIndex + 1)_03")
                 .resizable()
                 .scaledToFit()
                 .frame(width: (UIScreen.main.bounds.width)*2, height: (UIScreen.main.bounds.height)*2)
                 .offset(x: rightImageOffset, y: 0)
-                .animation(.easeInOut(duration: 1.5))
         }
-        .onAppear {
-            // 이동 후 고정되는 위치 값
-            leftImageOffset = -10
-            rightImageOffset = 10
+        .onAppear{
+            withAnimation(Animation.easeInOut(duration: 1.5)){
+                // 이동 후 고정되는 위치 값
+                leftImageOffset = -10
+                rightImageOffset = 10
+            }
+        }
+    }
+}
+
+struct BackOpacityAnimation:View{
+    @EnvironmentObject var stageViewModel: StageViewModel
+    var body: some View{
+        ZStack{
+            Image("StageBg0\(stageViewModel.selectedIndex + 1)").ignoresSafeArea()
         }
     }
 }
