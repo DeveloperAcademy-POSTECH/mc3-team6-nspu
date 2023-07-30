a29680b1f26b5877cb00609e842fe5745f46a4e1
‘youihyonKim’ <insidesy4@gmail.com>
2023-07-30 22:38:27 +0900

    feat/#80 - AlertView 생성

---
+++ OrrNyan/FloorUpdatedAlert.swift
@@ -0,0 +1,53 @@
+//
+//  FloorUpdatedAlert.swift
+//  OrrNyan
+//
+//  Created by YouiHyon Kim on 2023/07/30.
+//
+
+import SwiftUI
+
+struct FloorUpdatedAlert: View {
+	@State var isFloorUpdated : Bool = false
+    var body: some View {
+		
+		VStack(spacing: 0){
+			Text("대단해요!")
+				.font(.system(size: 17, weight: .semibold))
+			Text("잊지 않고 열심히 계단을 올랐군요?")
+				.font(.system(size:13, weight: .regular))
+				.padding(.top, 4)
+			
+			Divider().padding(EdgeInsets(top: 17, leading: 0, bottom: 12, trailing: 0))
+				.foregroundColor(Color.Gray200)
+			
+			
+			Button {
+				print("여기에 함수!")
+			} label: {
+				Text("+ 12층")
+					.font(.system(size: 17, weight: .bold))
+					.foregroundColor(Color.Purple300)
+					.frame(maxWidth: .infinity)
+			}
+		
+
+		}
+		.frame(maxWidth: .infinity)
+		.padding(EdgeInsets(top: 20, leading: 0, bottom: 12, trailing: 0))
+		.background(.thinMaterial)
+		.cornerRadius(14)
+		.padding(.horizontal, 60)
+		
+
+		
+		
+		
+    }
+}
+
+struct FloorUpdatedAlert_Previews: PreviewProvider {
+    static var previews: some View {
+        FloorUpdatedAlert()
+    }
+}
